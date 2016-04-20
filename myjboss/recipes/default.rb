0952f4a29b5cd291a0e750870456bb99fe8e2a33
MLapshyn <snooppdog@yandex.ru>
2016-04-20 10:57:29 +0300

    Update default.rb

--- myjboss/recipes/default.rb
+++ myjboss/recipes/default.rb
@@ -8,50 +8,50 @@
 #
 
 include_recipe 'java'
-#Installig yum
+#Yum
 yum_package 'unzip' do
   action :install
 end 
-#Download JBoss
-  remote_file "#{ node['jboss']['target_dir'] }/jboss-5.1.0.GA.zip" do
+#User
+user node['jboss']['jboss_user'] do
+  comment 'jboss user'
+  home node['jboss']['jboss_home']
+  system true
+  shell '/bin/bash'
+end
+#Group
+group node['jboss']['jboss_group'] do
+  action :create
+end
+#JBoss
+remote_file "#{ node['jboss']['target_dir'] }/jboss-5.1.0.GA.zip" do
   source node['jboss']['url']
   owner node['jboss']['jboss_user']
   group node['jboss']['jboss_group']
   mode '0755'
   action :create 
 end
-#USER
-  user node['jboss']['jboss_user'] do
-  comment 'jboss user'
-  home node['jboss']['jboss_home']
-  system true
-  shell '/bin/bash'
-end
-#GROUP
-  group node['jboss']['jboss_group'] do
-  action :create
-end
 #Extract JBoss
-  execute 'extract_jboss' do
+execute 'extract_jboss' do
   command 'unzip jboss-5.1.0.GA.zip'
   cwd node['jboss']['target_dir']
 end
 #APP
-  remote_file "#{ node['jboss']['target_dir'] }/testweb.zip" do
+remote_file "#{ node['jboss']['target_dir'] }/testweb.zip" do
   source node['jboss']['app_url']
   owner node['jboss']['jboss_user']
   group node['jboss']['jboss_group']
   mode '0755'
   action :create
 end
-#EXECUTE APP 
-  execute 'extract_testweb_zipfile' do
+#Extract app 
+execute 'extract_testweb_zipfile' do
   command "unzip testweb.zip -d #{ node['jboss']['deploy_path'] }"
   cwd node['jboss']['target_dir']
   not_if { File.directory?( "#{ node['jboss']['deploy_path']}/testweb" ) }
 end
-#INIT JBoss
-  template "/etc/init.d/jboss" do
+#Init JBoss
+template "/etc/init.d/jboss" do
   source "jboss.erb"
   owner 'root'
   group 'root'
@@ -61,12 +61,13 @@ end
      :jboss_home => node['jboss']['jboss_home'],
      :bind_address => node['jboss']['bind_address']
   })
-#OWNER
-  execute 'jboss folder ownership' do
+end
+#Owner
+execute 'jboss folder ownership' do
   command "chown -R #{ node['jboss']['jboss_user'] }:#{ node['jboss']['jboss_group'] } #{ node['jboss']['jboss_home'] }"
 end
 
-
+#Service
 service 'jboss' do
   init_command "/etc/init.d/jboss"
   supports :restart => true, :status => true, :stop => true
